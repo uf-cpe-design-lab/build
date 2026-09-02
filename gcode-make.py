597c40042613c1b1157dd8e4d45e72c0407dc342
LandenParke <eddisonparke@gmail.com>
2026-09-02 09:20:28 -0400

    Added exception handling for errors.

--- gcode-make.py
+++ gcode-make.py
@@ -38,19 +38,31 @@ cmd = [
 
 if support:
     cmd.extend(support.split())
-subprocess.run(cmd, capture_output=True, text=True)
+try:
+    subprocess.run(cmd, capture_output=True, text=True)
+except:
+    with open("comment", "x", encoding="utf-8") as f:
+        f.write("""
+Print Request Failed!
+----------------------------------------------
+There was either an issue with your .stl file or you submitted the wrong file!
+Please resubmit and ensure the following:
+1. Your file has the .stl extension
+2. You are submitting ONE file to be printed
+3. Your file is in a zip archive
+""")
+else:
+    # Time parsing
+    with open("output.gcode", "r") as f:
+        content = f.read()
 
-# Time parsing
-with open("output.gcode", "r") as f:
-    content = f.read()
-
-time = ""
-match = re.search(r"estimated printing time \(normal mode\) = (.+)", content)
-if match:
-    time = match.group(1)
-
-with open("comment", "x", encoding="utf-8") as f:
-    f.write(f"""
+    time = ""
+    match = re.search(r"estimated printing time \(normal mode\) = (.+)", content)
+    if match:
+        time = match.group(1)
+WebWork Accessibility
+    with open("comment", "x", encoding="utf-8") as f:
+        f.write(f"""
 Print request successful!
 ----------------------------------------------
 Your estimated print time is: {time}
